724ee0e94f5a88d8458fec5625e90ffb0588814f
Cosmodocus <homayunir@gmail.com>
2024-02-24 14:23:00 -0500

    index.css to style.css + and max-stat
    Changed css directory + max stat limit to 155

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -307,7 +307,7 @@ function displayPokemonDetails(pokemon) {
 		createAndAppendElement(statDiv, 'progress', {
 			className: 'progress-bar',
 			value: base_stat,
-			max: 255,
+			max: 155,
 		});
 	});
 
